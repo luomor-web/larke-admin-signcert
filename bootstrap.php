e8016253fce109c50f3d19e77e3ca3c0d75c9d8d
deatil <2217957370@qq.com>
2021-01-02 00:56:00 +0800

    扩展逻辑更新

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,4 @@
 
 use Larke\Admin\Facade\Extension;
 
-if (! class_exists("Larke\\Admin\\SignCert\\ServiceProvider")) {
-    Extension::namespaces([
-        'Larke\\Admin\\SignCert\\' => __DIR__ . '/src',
-    ]);
-}
-
 Extension::extend('larke/sign-cert', Larke\Admin\SignCert\ServiceProvider::class);
